36132ac19f601137f16815229aac51d7ea90f8cf
arlenavm95 <arlenavazquez95@gmail.com>
2026-01-05 16:41:30 +0100

    Send and read data to and from Firestore

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
 
 class NewMessage extends StatefulWidget {
@@ -10,7 +13,7 @@ class NewMessage extends StatefulWidget {
 }
 
 class _NewMessageState extends State<NewMessage> {
-  var _messageController = TextEditingController();
+  final _messageController = TextEditingController();
 
   @override
   void dispose() {
@@ -18,14 +21,29 @@ class _NewMessageState extends State<NewMessage> {
     super.dispose();
   }
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _messageController.text;
 
-    if (enteredMessage.trim().isEmpty){
+    if (enteredMessage.trim().isEmpty) {
       return;
     }
 
-    _messageController.clear();
+    FocusScope.of(context).unfocus();
+    _messageController.clear();  
+
+    final user = FirebaseAuth.instance.currentUser!;
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });      
   }
 
   @override
@@ -34,13 +52,15 @@ class _NewMessageState extends State<NewMessage> {
       padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
       child: Row(
         children: [
-          Expanded(child: TextField(
-            controller: _messageController,
-            textCapitalization: TextCapitalization.sentences,
-            autocorrect: true,
-            enableSuggestions: true,
-            decoration: const InputDecoration(labelText: 'Send a message...'),
-          )),
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: const InputDecoration(labelText: 'Send a message...'),
+            ),
+          ),
           IconButton(
             onPressed: _submitMessage,
             icon: const Icon(Icons.send),
